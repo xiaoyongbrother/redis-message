899a22677d469bbfff345bd0706ee27101ac7eb3
zxy <zhangxyai@163.com>
2017-04-01 19:40:03 +0800

    aaa

---
+++ demo/index.php
@@ -0,0 +1,10 @@
+<?php
+	require_once __DIR__.'/vendor/autoload.php';
+	
+	$object = new xiaoyongbrother\message\SinglePullMessage('127.0.0.1');
+	$sender = 'lisi';          //发送者
+	$to = 'wangwu';            //接收者
+	$message = 'How are you';  //信息
+	$time = time();
+	$arr=array('sender' => $sender, 'message' => $message, 'time' => $time);
+	echo $object->sendSingle($to, $arr);
